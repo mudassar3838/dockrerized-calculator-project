2640649b8edaed064f9fd38a9e0aa571365b2826
mudassar3838 <mudassardogar38@gmail.com>
2026-04-20 23:06:28 +0500

    get two number from user

--- calculator.py
+++ calculator.py
@@ -10,4 +10,6 @@ def divide(a, b):
     return a / b
 if __name__ == "__main__":
     print("simple calculator running in docker")
+    a= float(input("Enter first number: "))
+    b= float(input("Enter second number: "))
     
