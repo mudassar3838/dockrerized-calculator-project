1376032129045b5b3c75b57756151e69296d8dd7
mudassar3838 <mudassardogar38@gmail.com>
2026-04-20 18:27:04 +0500

    add new function

--- calculator.py
+++ calculator.py
@@ -6,7 +6,5 @@ def multiply(a, b):
     return a * b
 def divide(a, b):
     if b == 0:
-        raise ValueError("Cannot divide by zero")
+        return "Error: Division by zero is not allowed."
     return a / b
-
-print ("calculator running in Docker project")
