fce386f7ba5f3336c87262d9da2ebfcf005a4bea
mudassar3838 <mudassardogar38@gmail.com>
2026-04-21 09:36:23 +0500

    add error handling function

--- calculator.py
+++ calculator.py
@@ -10,10 +10,18 @@ def divide(a, b):
     return a / b
 if __name__ == "__main__":
     print("simple calculator running in docker")
+try:
+
     a= float(input("Enter first number: "))
     b= float(input("Enter second number: "))
+    
     print("addition:", add(a, b))
     print("subtraction:", subtract(a, b))
     print("multiplication:", multiply(a, b))
     print("division:", divide(a, b))
-    
+     
+except ValueError: 
+    print("error: please enter valid numbers")
+
+except Exception as e:
+    print("unexpected error:", e)    
